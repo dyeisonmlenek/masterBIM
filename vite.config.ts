fb91fed9d0b40fb1a38160d5aac71560f4abbba8
Dyeison Cesar Mlenek <dyeison.mlenek@gmail.com>
2026-08-13 11:39:42 -0300

    Fix GH Pages build and add workflow

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/masterBIM/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ["web-ifc", "@thatopen/components", "@thatopen/fragments", "@thatopen/ui", "@thatopen/ui-obc"]
