96e914ffd663f79635ba7279ac038e85b82ec761
Dyeison Cesar Mlenek <dyeison.mlenek@gmail.com>
2026-08-13 12:02:12 -0300

    fix: use BASE_URL for WASM path to support GitHub Pages subpath

--- src/bim-components/setup/src/ifc-loader.ts
+++ src/bim-components/setup/src/ifc-loader.ts
@@ -3,8 +3,8 @@ import * as OBC from "@thatopen/components"
 export const setupIfcLoader = async (components: OBC.Components) => {
   const ifcLoader = components.get(OBC.IfcLoader);
   ifcLoader.settings.wasm = {
-    path: "/",
+    path: import.meta.env.BASE_URL,
     absolute: true
   };
   await ifcLoader.setup({ autoSetWasm: false });
-}
+}
